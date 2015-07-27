029329912fa06a26746d9b9f10511aa02914fe54
Alin Kokarca <alin.kokarca@endava.com>
2015-07-27 17:01:07 +0300

    add and error

--- HelloWord/HelloWord/Program.cs
+++ HelloWord/HelloWord/Program.cs
@@ -12,7 +12,7 @@ namespace HelloWord
 
         private static void Main(string[] args)
         {
-            Console.WriteLine(CreateMessage());
+            Console.WriteLine(CreateMessage())
         }
 
         public static string CreateMessage()
