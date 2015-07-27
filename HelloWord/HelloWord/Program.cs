450e45b80a35afec019c36778441f5b2b63d955f
Alin Kokarca <alin.kokarca@endava.com>
2015-07-27 15:33:23 +0300

    first commit

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        public Program() { }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return ("HelloWord");
+        }
+    }
+}
